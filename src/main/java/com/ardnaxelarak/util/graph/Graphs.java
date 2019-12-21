e30b84ed34f1c6ca8c1378c11f3e60c5c5fb5374
Kara Alexandra <ardnaxelarak@gmail.com>
2019-12-21 02:31:09 -0800

    Add methods to Graphs to find all distances from one or more starting locations

--- src/main/java/com/ardnaxelarak/util/graph/Graphs.java
+++ src/main/java/com/ardnaxelarak/util/graph/Graphs.java
@@ -1,5 +1,8 @@
 package com.ardnaxelarak.util.graph;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableTable;
+import com.google.common.collect.Table;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
@@ -32,7 +35,7 @@ public final class Graphs {
         }
 
         long dist = baseDist + edge.getCost();
-        
+
         if (!unvisited.containsKey(newT) || unvisited.get(newT) > dist) {
           unvisited.put(newT, dist);
         }
@@ -40,6 +43,82 @@ public final class Graphs {
     }
 
     return Long.MAX_VALUE;
+  }
+
+  public static <T> Map<T, Long> getDistancesFrom(EdgeProvider<T> edges, T start) {
+    Map<T, Long> visited = new HashMap<>();
+    Map<T, Long> unvisited = new HashMap<>();
+    unvisited.put(start, 0L);
+
+    while (!unvisited.isEmpty()) {
+      T cur =
+          unvisited.entrySet().stream()
+              .min(Comparator.comparingLong(Map.Entry::getValue)).get().getKey();
+
+      long baseDist = unvisited.get(cur);
+      visited.put(cur, baseDist);
+      unvisited.remove(cur);
+
+      Iterable<Edge<T>> curEdges = edges.getEdges(cur);
+      for (Edge<T> edge : curEdges) {
+        T newT = edge.getValue();
+
+        if (visited.containsKey(newT)) {
+          continue;
+        }
+
+        long dist = baseDist + edge.getCost();
+
+        if (!unvisited.containsKey(newT) || unvisited.get(newT) > dist) {
+          unvisited.put(newT, dist);
+        }
+      }
+    }
+
+    return ImmutableMap.copyOf(visited);
+  }
+
+  public static <T> Table<T, T, Long> getAllDistances(
+      EdgeProvider<T> edges, Iterable<T> starts) {
+    ImmutableTable.Builder<T, T, Long> distances = ImmutableTable.builder();
+    Map<T, Long> visited = new HashMap<>();
+    Map<T, Long> unvisited = new HashMap<>();
+
+    for (T start : starts) {
+      visited.clear();
+      unvisited.clear();
+      unvisited.put(start, 0L);
+
+      while (!unvisited.isEmpty()) {
+        T cur =
+            unvisited.entrySet().stream()
+                .min(Comparator.comparingLong(Map.Entry::getValue)).get().getKey();
+
+        long baseDist = unvisited.get(cur);
+        visited.put(cur, baseDist);
+        unvisited.remove(cur);
+        if (!start.equals(cur)) {
+          distances.put(start, cur, baseDist);
+        }
+
+        Iterable<Edge<T>> curEdges = edges.getEdges(cur);
+        for (Edge<T> edge : curEdges) {
+          T newT = edge.getValue();
+
+          if (visited.containsKey(newT)) {
+            continue;
+          }
+
+          long dist = baseDist + edge.getCost();
+
+          if (!unvisited.containsKey(newT) || unvisited.get(newT) > dist) {
+            unvisited.put(newT, dist);
+          }
+        }
+      }
+    }
+
+    return distances.build();
   }
 
   private Graphs() {}
